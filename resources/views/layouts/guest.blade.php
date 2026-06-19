9282eb2b436a18ae9e61cb1b57c58db26280326a
Ivanderjexsen <ivanderjexsen@gmail.com>
2026-06-19 19:16:53 +0700

    Update Tampilan

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,28 @@
 
     <title>@yield('title')</title>
 
+    <link rel="preconnect"
+      href="https://fonts.googleapis.com">
+
+    <link rel="preconnect"
+      href="https://fonts.googleapis.com">
+
+      <link rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin>
+
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+      rel="stylesheet">
+
+      <link href="{{ asset('css/custom.css') }}"
+      rel="stylesheet">
+
+    <link rel="preconnect"href="https://fonts.gstatic.com"
+      crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet">
+
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}"
           rel="stylesheet">
 
@@ -15,18 +37,19 @@
 
 </head>
 
-<body class="bg-gradient-primary">
+<body>
 
-<div class="container">
+    <div class="circle circle1"></div>
+    <div class="circle circle2"></div>
+    <div class="circle circle3"></div>
+    <div class="circle circle4"></div>
 
-    @yield('content')
-
-</div>
+    <div class="auth-container">
+        @yield('content')
+    </div>
 
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
 </body>
